e3917a7fd4c1e7bd9b76da9cdab11c97a53b66e4
yourwifelover <vasyashinder.ua@gmail.com>
2026-04-30 23:13:07 +0300

    some part of cursach

---
+++ seventeenthtry/seventeenthtry/APIKEY.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace APIKEY
+{
+    class API
+    {
+        public readonly string apiKey = "d28296baee09cb85b22c0454f591b994";
+        public API() { }
+    }
+
+}
